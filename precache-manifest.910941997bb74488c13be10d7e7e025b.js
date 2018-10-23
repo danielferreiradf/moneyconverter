c82f739ea9d5b40ab83b0c2c8f7acbd4c357262e
danielferreiradf <danielferreiradk@gmail.com>
2018-10-23 13:26:09 -0300

    Updates

---
+++ precache-manifest.910941997bb74488c13be10d7e7e025b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c96eaca70b872d7a08a14e567a8d199",
+    "url": "/moneyconverter/static/media/logo.7c96eaca.png"
+  },
+  {
+    "revision": "af6b07174dfb603a3ee7",
+    "url": "/moneyconverter/static/js/runtime~main.af6b0717.js"
+  },
+  {
+    "revision": "fe0930eb9c4ea6f02c59",
+    "url": "/moneyconverter/static/js/main.fe0930eb.chunk.js"
+  },
+  {
+    "revision": "5e6ffcb8d5b0d6b62798",
+    "url": "/moneyconverter/static/js/1.5e6ffcb8.chunk.js"
+  },
+  {
+    "revision": "fe0930eb9c4ea6f02c59",
+    "url": "/moneyconverter/static/css/main.3680bc2c.chunk.css"
+  },
+  {
+    "revision": "a95c88fbc974510f879712c9fae132ac",
+    "url": "/moneyconverter/index.html"
+  }
+];
